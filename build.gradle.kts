14f217a204eda11d30ccdca2b708bed3c09cd503
shinyay <shinya.com@gmail.com>
2020-04-10 23:51:37 +0900

    Modify Spring Plugin version as 1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.2.71"
-    kotlin("plugin.spring") version "1.2.71"
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.spring") version "1.3.71"
     id("com.google.cloud.tools.jib") version "1.8.0"
 }
 
